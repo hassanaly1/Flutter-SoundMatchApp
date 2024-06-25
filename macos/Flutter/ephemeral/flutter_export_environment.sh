083e324645870c437b781cd81b01c58709835992
hassanaly1 <hassanali1.dev@gmail.com>
2024-06-25 15:05:54 +0500

    App Dependencies Upgraded.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\HA\sound_app"
+export "FLUTTER_ROOT=C:\Users\Progziel\Downloads\Compressed\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Progziel\Documents\Flutter-SoundMatchApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
